b92e33f21f73af0287f995d271d82c158ced3c98
tikskit <tikskit@gmail.com>
2026-08-15 22:36:59 +0700

    Initial commit

---
+++ authbasics/src/main/java/ru/vluzhnykh/authbasics/AuthbasicsApplication.java
@@ -0,0 +1,13 @@
+package ru.vluzhnykh.authbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthbasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthbasicsApplication.class, args);
+	}
+
+}
---
+++ authbasics/src/test/java/ru/vluzhnykh/authbasics/AuthbasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vluzhnykh.authbasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthbasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
